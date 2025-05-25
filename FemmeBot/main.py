13a2f3a44ba22abc3eb5c0ad72b1d539eefa4623
AmberFierce <amberrosemiles2410@gmail.com>
2025-05-25 23:05:54 +0100

    Update main.py

--- FemmeBot/main.py
+++ FemmeBot/main.py
@@ -371,6 +371,7 @@ async def setup(ctx, subcommand=None, *args):
         try:
             message = await ctx.channel.fetch_message(int(message_id))
             reaction_roles.setdefault(str(ctx.guild.id), {}).setdefault(message_id, {})[emoji] = role_name
+	    await add_reaction_role(ctx.guild.id, message_id, emoji, role_name)
             await message.add_reaction(emoji)
             await ctx.send(f"✅ Reaction role added: {emoji} → {role_name}")
         except Exception as e:
